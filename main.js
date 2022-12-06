fe8a8a0b80964005cedead963256b60a50aecac1
Princechigozie11 <goziep83@gmail.com>
2022-12-06 16:31:31 +0100

    Updated Basketball Project

--- main.js
+++ main.js
@@ -18,16 +18,22 @@ buttons.forEach((button) => {
   button.addEventListener("click", () => {
     if (button.classList.contains("btn1")) {
       homeDisplay += 1;
-      homeTitleColor();
       displayHome.textContent = homeDisplay;
     } else if (button.classList.contains("btn2")) {
       homeDisplay += 2;
-      homeTitleColor();
       displayHome.textContent = homeDisplay;
     } else if (button.classList.contains("btn3")) {
       homeDisplay += 3;
-      homeTitleColor();
       displayHome.textContent = homeDisplay;
+    }
+    if (homeDisplay > guestDisplay) {
+      guest.style.color = "white";
+      homeTitleColor();
+    } else if (homeDisplay < guestDisplay) {
+      guestTitleColor();
+      home.style.color = "white";
+    } else if (homeDisplay === guestDisplay) {
+      guest.style.color = "white";
     }
   });
 });
@@ -36,23 +42,29 @@ buttons.forEach((button) => {
   button.addEventListener("click", () => {
     if (button.classList.contains("guestBtn1")) {
       guestDisplay += 1;
-      guestTitleColor();
       displayGuest.textContent = guestDisplay;
     } else if (button.classList.contains("guestBtn2")) {
       guestDisplay += 2;
-      guestTitleColor();
       displayGuest.textContent = guestDisplay;
     } else if (button.classList.contains("guestBtn3")) {
       guestDisplay += 3;
-      guestTitleColor();
       displayGuest.textContent = guestDisplay;
+    }
+    if (homeDisplay > guestDisplay) {
+      guest.style.color = "white";
+      homeTitleColor();
+    } else if (homeDisplay < guestDisplay) {
+      guestTitleColor();
+      home.style.color = "white";
+    } else if (homeDisplay === guestDisplay) {
+      home.style.color = "white";
     }
   });
 });
 
 newButton.addEventListener("click", () => {
-  home.style.color = "";
-  guest.style.color = "";
+  home.style.color = "white";
+  guest.style.color = "white";
   homeDisplay = 0;
   guestDisplay = 0;
   displayHome.textContent = homeDisplay;
